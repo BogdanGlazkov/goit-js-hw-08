73022f05966da219c3425a5d9e5905c5362e5ec6
BogdanGlazkov <bglazkov@i.ua>
2022-08-15 18:04:17 +0300

    Completed

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,24 +1,26 @@
+import { galleryItems } from './gallery-items.js';
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
-// Add imports above this line
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-console.log('hello');
 const galleryEl = document.querySelector('.gallery');
 
-const galleryMarkup = galleryItems
-  .map(({ preview, original, description }) => {
-    return `
-    <a class="gallery__item" href="${original}">
-    <img class="gallery__image" src="${preview}" alt="${description}" />
-    </a>`;
-  })
-  .join('');
+galleryEl.insertAdjacentHTML('beforeend', renderGalleryItems());
 
-galleryEl.insertAdjacentHTML('beforeend', galleryMarkup);
+function renderGalleryItems() {
+  return galleryItems
+    .map(
+      ({ original, preview, description }) =>
+        `<div class="gallery__item">
+            <a class="gallery__link" href="${original}">
+             <img class="gallery__image" src="${preview}" alt="${description}" />
+            </a>
+        </div>`
+    )
+    .join('');
+}
 
 new SimpleLightbox('.gallery a', {
   captionsData: 'alt',
   captionDelay: 250,
+  scrollZoomFactor: false,
 });
